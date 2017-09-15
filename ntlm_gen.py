3668133af3b328b0ab6dc67c63fafffbfbd716f4
Tyler Price (CPT) <tprice@e-complish.com>
2017-09-15 16:18:24 -0400

    Create ntlm_gen.py

---
+++ ntlm_gen.py
@@ -0,0 +1,23 @@
+import hashlib,binascii
+import sys
+
+def generate():
+
+	if len(sys.argv) < 2:
+
+		print "usage: ntml_gen.py file.txt"
+
+		sys.exit(1)
+
+	file = sys.argv[1]
+
+	f = open(file, 'r')
+
+	for word in iter(f):
+
+		hash = hashlib.new('md4', word.encode('utf-16le')).digest()
+		print binascii.hexlify(hash)
+
+	f.close()
+
+generate()
